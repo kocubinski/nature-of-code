2c6723a52b4d23e87c900d535fe7465e18c4f73e
Matt Kocubinski <mkocubinski@gmail.com>
2015-05-12 19:38:08 -0400

    Finish this example of friction.

--- js/2_Forces/friction.js
+++ js/2_Forces/friction.js
@@ -9,12 +9,13 @@ function Mover(mass, x, y) {
 }
 
 Mover.prototype.draw = function(sketch) {
-    sketch.circle(this.size, this.location, {color: "green"});
+    sketch.circle(this.size, this.location, {color: "yellow"});
 };
 
 Mover.prototype.update = function() {
     this.location.add(this.velocity);
     this.velocity.add(this.acceleration);
+    this.velocity.limit(10);
     this.acceleration.mult(0);
 };
 
@@ -25,7 +26,7 @@ Mover.prototype.applyForce = function(force) {
 
 Mover.prototype.checkEdges = function(sketch) {
     var s = sketch;
-    var bouncyness = 1;
+    var bouncyness = 2;
 
     if (this.location.x > s.width) {
         this.location.x = s.width;
@@ -40,7 +41,7 @@ Mover.prototype.checkEdges = function(sketch) {
         this.velocity.y *= -bouncyness;
     } else if (this.location.y < 0) {
         this.location.y = 0;
-        this.velocity.y *= -bouncyness;
+        this.velocity.y *= -bouncyness / 2;
     }
 };
 
@@ -53,14 +54,14 @@ function setup() {
 
     for (var i = 0; i < 20; i++) {
         var mass = 20 * Math.random();
-        if (mass < 0.5) mass = 0.5;
+        if (mass < 1) mass = 1;
         ms.push(new Mover(mass, 30, 30));
     }
 
     if (s) { s.destroy(); };
     s = new Sketch.sketch('canvas', 900, 600);
     //s.background('black');
-    //s.onTick = draw;
+    s.onTick = draw;
 
     noise.seed(Math.random());
     frictionMap = noiseMap(width, height);
@@ -68,21 +69,33 @@ function setup() {
 }
 
 function drawNoise(map) {
-    // var image = s.ctx.createImageData(s.width, s.height);
-    // var data = image.data;
-    // for (var x = 0; x < map.length; x++) {
-    //     var row = map[x];
-    //     for (var y = 0; y < row.length; y++) {
-    //         var value = Math.floor(row[y] * 256);
-    //         var cell = (x + y * map.length) * 4;
-    //         data[cell] = data[cell + 1] = data[cell + 2] = value;
-    //         data[cell + 3] = 255;
-    //     }
-    // }
+    var image = s.ctx.createImageData(s.width, s.height);
+    var data = image.data;
+    for (var x = 0; x < map.length; x++) {
+        var row = map[x];
+        for (var y = 0; y < row.length; y++) {
+            var value = Math.abs(Math.floor(row[y] * 256));
+            var cell = (x + y * map.length) * 4;
+            data[cell] = data[cell + 1] = data[cell + 2] = value;
+            data[cell + 3] = 255;
+        }
+    }
 
-    // s.ctx.putImageData(image, 0, 0);
+    s.ctx.putImageData(image, 0, 0);
+}
 
-
+function getFriction(x, y) {
+    x = Math.floor(x);
+    x = isNaN(x) ? 0 : x;
+    x = x == s.width ? x - 1 : x;
+    y = Math.floor(y);
+    y = isNaN(y) ? 0 : y;
+    //console.log(x, y);
+    var f = Math.abs(frictionMap[x][y]);
+    f = isNaN(f) ? 0 : f;
+    f = f / 1.1;
+    //console.log(f);
+    return f;
 }
 
 function noiseMap(w, h) {
@@ -100,16 +113,21 @@ function noiseMap(w, h) {
 
 function draw(sketch) {
     sketch.clear();
+    drawNoise(frictionMap);
 
     var wind = new Sketch.vector2(0.02, 0);
     var gravity = new Sketch.vector2(0, 0.3);
 
     for (var i = 0; i < ms.length; i++) {
-        var c = 0.01;
+        var m = ms[i];
+
+        //var c = 0.01;
+
+        var c = getFriction(m.location.get().x,
+                            m.location.get().y);
         var normal = 1;
         var frictionMag = c * normal;
 
-        var m = ms[i];
         var friction = m.velocity.get();
         friction.mult(-1);
         friction.normalize();
